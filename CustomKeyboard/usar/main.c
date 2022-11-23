4c11606e9dafab4d3daff8ca501f1271783acbfa
TabbyCatPie <78999461+TabbycatPie@users.noreply.github.com>
2022-11-23 21:07:46 +0800

    Update main.c

--- CustomKeyboard/usar/main.c
+++ CustomKeyboard/usar/main.c
@@ -3,15 +3,36 @@
 #include "DataFlash.h"
 #include "usb.h"
 
+#define uchar = unsiged char
 
-HIDMouse
+#define L_KEY_MASK 0x01  // left key press mask
+#define R_KEY_MASK 0x02  // right key press mask
+#define M_KEY_MASK 0x04  // middle key press mask
+#define X_N_MASK 0x10    // 1 for X axis negative
+#define Y_N_MASK 0x20    // 1 for Y axis negative
+#define X_O_MASK 0x40    // 1 for X axis overflow
+#define Y_O_MASK 0x80    // 1 for Y axis overflow
+//direction
+#define GO_LEFT  0x01
+#define GO_RIGHT 0x02 
+#define GO_UP    0x04 
+#define GO_DOWN  0x08
+
+void MoveMouse(char direction,char val){
+	memset(HIDMouse,0,sizeof(HIDMouse));
+	HIDMouse[0] |= 0x08;
+	if(direction & GO_LEFT == GO_LEFT){
+		HIDMouse[0] |= X_N_MASK;
+	}
+	if(direction & GO_UP == GO_UP){
+		HIDMouse[0] |= Y_N_MASK;
+	}
+}
 
 void HIDMousesend(){
-	if(CUR_MOUSE_KEY == 0xff && KEY_CHANGE == 0xff){
-		FLAG = 0;
-		Mouse_Send();    //if mouse key is set,then send mouse event
-		while(FLAG == 0); 
-	}
+	FLAG = 0;
+	Mouse_Send();    //if mouse key is set,then send mouse event
+	while(FLAG == 0); 
 }
 
 
@@ -20,7 +41,6 @@ void main(){
 	CfgFsys();                    //CH552时钟选择配置
 	mDelaymS(50);                 //修改主频等待内部晶振稳定,必加
 	USBDeviceInit();              //USB设备模式初始化
-	initKeyValue();              	//intialize key 
 	EA = 1;                       //允许单片机中断
 
 	while(1)
